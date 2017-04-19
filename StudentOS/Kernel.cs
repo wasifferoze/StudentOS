1cb7870204701331dcdbe54a37a4a3a75a54595b
BitLord <wasif_feroze@yahoo.com>
2017-04-19 20:17:40 +0800

    first commit

---
+++ StudentOS/Kernel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace StudentOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
